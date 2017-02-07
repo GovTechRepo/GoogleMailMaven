1bad3526473abe357aeda316bbebbcf68c142092
IDACTMO002 <IDACTMO002@GOVTECH-CTMO-02>
2017-02-07 10:27:55 +0800

    Signed-off-by: IDACTMO002 <IDACTMO002@GOVTECH-CTMO-02>

--- src/test/java/eMail/GmailDemo_InValidLogin.java
+++ src/test/java/eMail/GmailDemo_InValidLogin.java
@@ -1,14 +1,21 @@
 package eMail;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.xpath.operations.Equals;
 import org.eclipse.jdt.internal.compiler.ast.AssertStatement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -37,22 +44,34 @@ public class GmailDemo_InValidLogin {
 
 		driver.findElement(By.cssSelector("#Email")).clear();
 		driver.findElement(By.cssSelector("#Email")).sendKeys("govtechtesting");
-			
+
 		driver.findElement(By.xpath("//input[contains(@value,'Nextt')]")).click();
-												
+
 		driver.findElement(By.cssSelector("#Passwd")).clear();
 		driver.findElement(By.cssSelector("#Passwd")).sendKeys("govtechtesting11");
-									
+
 		driver.findElement(By.cssSelector("#signIn")).click();
-		
+
 		WebElement element = driver.findElement(By.xpath("//span[@id='errormsg_0_Passwd']"));
 		String strng = element.getText();
 		System.out.println(strng);
 		Assert.assertEquals("Wrong password. Try again.", strng);
 
 		System.out.println(driver.getTitle());
-		
-		driver.quit();
+
+	}
+
+	@AfterMethod
+	public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
+		if (testResult.getStatus() == ITestResult.FAILURE) {
+			System.out.println(testResult.getStatus());
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			//FileUtils.copyFile(scrFile, new File("C:\\Users\\IDACTMO002\\Desktop\\PPT_310117"));
+			
+			String Failure = null;
+			String FailureScreenShotName = Failure ;
+			FileUtils.copyFile(scrFile, new File(".//test-output//Screenshots//"+FailureScreenShotName+".png"));
+	   }        
 	}
 
 	@BeforeTest
